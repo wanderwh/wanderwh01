5b2a493c2a06204099c9a98dbb4927ffdbd9c9ba
wanghuang <wanderhuang@163.com>
2016-04-18 00:17:44 +0800

    第一次提交

---
+++ base/src/main/java/com/wanderwh/base/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.wanderwh.base.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@ResponseBody
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+}
